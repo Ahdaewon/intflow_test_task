3fb033ef1cfb50345092943c544a917a35be9238
Ad1_one <ah237800@gmail.com>
2024-02-27 17:19:25 +0900

    Fix :  메인 화면 스와이프시 애니매이션으로 인한 오버프로우 개선

--- lib/root_screen/main_screen.dart
+++ lib/root_screen/main_screen.dart
@@ -115,13 +115,91 @@ class _MainScreenState extends State<MainScreen> {
             child: Column(
               children: [
                 AnimatedContainer(
-                  height: _isOrangeContainerVisible
-                      ? 200
-                      : 0, // 변경된 높이에 따라 애니메이션 적용
-                  duration: const Duration(milliseconds: 500), // 애니메이션 지속 시간
-                  curve: Curves.easeInOut, // 애니메이션 곡선 설정
+                  height: _isOrangeContainerVisible ? 200 : 0,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
                   color: const Color(0x00000000),
-                  child: const Center(child: Text("ㅁㅇ")), // 영역 가운데 정렬
+                  child: FutureBuilder(
+                    future: Future.delayed(Duration(milliseconds: 600)), // 약간의 딜레이
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                SizedBox(height: 10,),
+                                Text(
+                                  "성오 농자주님",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 24,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                                Row(
+                                  children: [
+                                    Text("반갑습니다" , style :  TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                    ) ),
+                                    Image.asset(
+                                      'assets/main_content_icon/hand_icon.png', width: 32, height: 32,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/main_content_icon/temp_icon.png',
+                                    ),
+                                    SizedBox(width: 10,),
+                                    Text("27℃" ,style: TextStyle(fontSize: 14, color: Colors.white ),)
+                                  ],
+                                ), Image.asset(
+                                  'assets/main_content_icon/seperate_icon.png',
+                                ),Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/main_content_icon/rain_icon.png',
+                                    ),
+                                    SizedBox(width: 10,),
+                                    Text("87%" ,style: TextStyle(fontSize: 14, color: Colors.white ),)
+                                  ],
+                                ),
+                                Image.asset(
+                                  'assets/main_content_icon/seperate_icon.png',
+                                ),
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/main_content_icon/co2_icon.png',
+                                    ),
+                                    SizedBox(width: 10,),
+                                    Text("500ppm" ,style: TextStyle(fontSize: 14, color: Colors.white ),)
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      } else {
+                        // 딜레이 중에 보여줄 로딩 등의 위젯
+                        return Container(
+                            width: 10, height: 10,
+                            child: CircularProgressIndicator()); // 예시로 로딩 인디케이터
+                      }
+                    },
+                  ),
                 ),
                 Expanded(
                     child: Container(
